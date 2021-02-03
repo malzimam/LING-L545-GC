aef249e24fc35fcee1e82d45055a3676658039b3
malzimam <malzimam@iu.edu>
2021-02-03 13:59:06 -0500

    djhjfh

---
+++ 00_Corpus/xx.py
@@ -0,0 +1,19 @@
+
+import sys
+
+b = sys.stdin.read()
+
+k=b.replace('.' , ' .')
+k=k.replace(',' , ' , ')
+k=k.replace('(' , ' (')
+k=k.replace(')' , ' )')
+k=k.replace('/', '/  ')
+k=k.replace('\\', '\ ')
+k=k.replace(':' , ' :')
+k=k.replace('and' , ' and')
+for sentencs in k.split('\n'):
+        print(sentencs)
+        for token in sentencs.split(' '):
+                lista = []
+                lista.append([token])
+                print(token,'\t_\t_\t_\t_\t_\t_\t_\t_\t_')
